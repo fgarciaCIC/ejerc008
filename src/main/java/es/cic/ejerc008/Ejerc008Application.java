764f14f5c8d4eac89b4b209f861521d4e5327133
fgarciaCIC <fgarcia@powereys.es>
2023-08-04 13:08:51 +0200

    Inicio proyecto 8

---
+++ src/main/java/es/cic/ejerc008/Ejerc008Application.java
@@ -0,0 +1,13 @@
+package es.cic.ejerc008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc008Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc008Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/ejerc008/Ejerc008ApplicationTests.java
@@ -0,0 +1,16 @@
+package es.cic.ejerc008;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc008ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
